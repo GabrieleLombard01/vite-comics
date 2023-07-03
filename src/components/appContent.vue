0da532c841356d54a79da41a32f78b9f611194cc
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-03 20:37:25 +0200

    cards added

--- src/components/appContent.vue
+++ src/components/appContent.vue
@@ -7,18 +7,38 @@ export default {
 </script>
 
 <template>
-    <div class="jumbo">
-        <div class="container">
-            <h2>CONTENT GOES HERE</h2>
+    <div class="content">
+        <div class="container card-container">
+            <div v-for="card in products" :key="card.series" class="card">
+                <img :src="card.thumb" :alt="card.series">
+                <h3>{{ card.series }}</h3>
+            </div>
+
         </div>
     </div>
 </template>
 
 <style scoped>
-.jumbo {
+.content {
     min-height: 90px;
     background-color: #1c1c1c;
     padding-top: 20px;
+}
+
+.card-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 2rem;
+}
+
+.card {
+    flex-basis: 15%;
+    padding: 20px;
+}
+
+.card img {
+    display: block;
+    width: 300px;
 }
 
 /*...*/
